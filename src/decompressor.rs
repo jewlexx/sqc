c6120deaea75470349ccffe19ca6000fdc7b7cda
Juliette Cordor <grassojames5@gmail.com>
2023-01-02 21:18:46 +1100

    fix

--- src/decompressor.rs
+++ src/decompressor.rs
@@ -1,4 +1,4 @@
-use std::io::{Read, Seek};
+use std::io::BufRead;
 
 pub struct Decompressor<R> {
     // TODO: Maybe don't use string because we might have to represent data larger than one's
@@ -8,15 +8,18 @@ pub struct Decompressor<R> {
 
 // Test if mem size of Read.iter() is bearable with large Read
 
-impl<R: Read + Seek> Decompressor<R> {
+impl<R: BufRead> Decompressor<R> {
     pub fn new(compressed: R) -> Self {
         Self { compressed }
     }
 
-    pub fn decompress(&mut self) -> std::io::Result<()> {
-        let mut bytes = self.compressed.bytes();
+    pub fn decompress(mut self) -> std::io::Result<()> {
+        loop {
+            let mut line = Vec::new();
+            self.compressed.read_until(b'\n', &mut line)?;
+        }
 
-        while let Some(byte) = bytes.next() {}
+        Ok(())
     }
 }
 
